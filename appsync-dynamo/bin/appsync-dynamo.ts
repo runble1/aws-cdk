80a329b7bb049026e2ab15eefe4c3d59cdbbbe52
runble1 <runble1@gmail.com>
2024-03-16 17:18:14 +0900

    released

--- appsync-dynamo/bin/appsync-dynamo.ts
+++ appsync-dynamo/bin/appsync-dynamo.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AppsyncDynamoStack } from '../lib/appsync-dynamo-stack';
+import { MyAppsyncDynamodbStack } from '../lib/appsync-dynamo-stack';
 
 const app = new cdk.App();
-new AppsyncDynamoStack(app, 'AppsyncDynamoStack', {
+new MyAppsyncDynamodbStack(app, 'AppsyncDynamoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- appsync-dynamo/lib/appsync-dynamo-stack.ts
+++ appsync-dynamo/lib/appsync-dynamo-stack.ts
@@ -1,16 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as appsync from 'aws-cdk-lib/aws-appsync'; // 修正
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
-export class AppsyncDynamoStack extends cdk.Stack {
+export class MyAppsyncDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDBテーブルの作成
+    const table = new dynamodb.Table(this, 'MyTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncDynamoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // AppSync APIの作成
+    const api = new appsync.GraphqlApi(this, 'Api', {
+      name: 'my-api',
+      schema: appsync.SchemaFile.fromAsset('schema/schema.gql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    // DynamoDBをデータソースとしてAppSync APIに追加
+    const dataSource = api.addDynamoDbDataSource('MyDataSource', table);
+
+    // リゾルバーの追加 (例: Query.getTodo)
+    dataSource.createResolver('QueryGetDemosResolver', {
+      typeName: 'Query',
+      fieldName: 'getTodo',
+      requestMappingTemplate: appsync.MappingTemplate.dynamoDbGetItem('id', 'id'),
+      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
+    });
   }
 }
