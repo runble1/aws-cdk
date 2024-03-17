2a7711866a2614d65a652a804b25488c5e61628c
runble1 <runble1@gmail.com>
2024-03-17 10:14:44 +0900

    first

--- lambda-appsync-dynamo/lib/resources/dynamodbTables.ts
+++ lambda-appsync-dynamo/lib/resources/dynamodbTables.ts
@@ -4,10 +4,18 @@ import { Construct } from 'constructs';
 
 export class DynamoDbTables {
   public readonly productTable: dynamodb.Table;
+  public readonly historyTable: dynamodb.Table;
 
   constructor(scope: Construct) {
     this.productTable = new dynamodb.Table(scope, 'ProductTable', {
       tableName: 'Product',
+      partitionKey: { name: 'productId', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.historyTable = new dynamodb.Table(scope, 'HistoryTable', {
+      tableName: 'History',
       partitionKey: { name: 'productId', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
