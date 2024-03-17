1a77b0cc3e755e7f7a5834ad8dccf9ce582e38af
runble1 <runble1@gmail.com>
2024-03-17 09:01:53 +0900

    add

--- appsync-dynamo/bin/appsync-dynamo.ts
+++ appsync-dynamo/bin/appsync-dynamo.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MyAppsyncDynamodbStack } from '../lib/appsync-dynamo-stack';
+import { MyAppsyncDynamodbStack } from '../lib/appsyncDynamoStack';
 
 const app = new cdk.App();
 new MyAppsyncDynamodbStack(app, 'AppsyncDynamoStack', {
